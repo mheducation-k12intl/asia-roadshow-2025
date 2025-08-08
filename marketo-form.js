b1f394fb9966c7244aa783d378bd836edf60a1c3
rudy-galera <rudy.galera@mheducation.com>
2025-08-08 20:38:36 +0400

    Create marketo-form.js

---
+++ marketo-form.js
@@ -0,0 +1,1 @@
+<script src="//info.mheducation.com/js/forms2/js/forms2.min.js"></script> <form id="mktoForm_22559"></form> <script>MktoForms2.loadForm("//info.mheducation.com", "128-SJW-347", 22559);</script>
